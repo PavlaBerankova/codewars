a0e2450f49e4beaad2e5e9933dab6d85811fc85b
Pavla Beránková <berankovap@protonmail.com>
2024-02-17 08:19:36 +0100

    [8KYU] Add 14_reduce in Swift and JavaScript

---
+++ Codewars/8KYU_Swift_JavaScript/14_reduce/reduce.swift
@@ -0,0 +1,11 @@
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+func grow(_ arr: [Int]) -> Int {
+  return arr.reduce(1, *)
+}
+
+print(grow([1, 2, 3, 4]))
